5d8cfdaf6f0438e8d83645794b5fd2c3b7edf827
Michal <mick@transcendstudio.pl>
2020-10-11 21:15:17 +0200

    sticky menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,17 @@
-
 const hamburger = document.querySelector(".hamburger--js");
 hamburger.addEventListener("click", (e) => {
-    document.querySelector(".navigation--js").classList.toggle("navigation--open");
-  });
+  document
+    .querySelector(".navigation--js")
+    .classList.toggle("navigation--open");
+});
+
+
+window.addEventListener('scroll', () => {
+  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
+  if (scrolled >= 100) document.querySelector(".main-header").classList.add("shrink-and-sticky");
+  else document.querySelector(".main-header").classList.remove("shrink-and-sticky");
+});
+
+
+
